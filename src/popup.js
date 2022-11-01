9d206a0e5698fcd705fffe415d7bca7751ef28a6
Vulpelo <damian.praski@gmail.com>
2022-11-01 12:03:54 +0100

    -fix manifest: 64px image was assigned tp 32 -Changed innerHtml to textContent

--- src/popup.js
+++ src/popup.js
@@ -21,11 +21,11 @@ window.onload = function() {
 
 
     // Set language
-    document.getElementById("hide_videos_text").innerHTML=chrome.i18n.getMessage("cfg_hide_videos");
-    document.getElementById("hide_tab_text").innerHTML=chrome.i18n.getMessage("cfg_hide_tab");
-    document.getElementById("settingsText").innerHTML=chrome.i18n.getMessage("setting_text");
+    document.getElementById("hide_videos_text").textContent=chrome.i18n.getMessage("cfg_hide_videos");
+    document.getElementById("hide_tab_text").textContent=chrome.i18n.getMessage("cfg_hide_tab");
+    document.getElementById("settingsText").textContent=chrome.i18n.getMessage("setting_text");
 
     // version
     let manifestData = chrome.runtime.getManifest();
-    document.getElementById("ext-version").innerHTML = "v" + manifestData.version;
+    document.getElementById("ext-version").textContent = "v" + manifestData.version;
 };
