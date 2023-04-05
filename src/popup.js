4cd35bce399669d3a09062e7b1a47e6eb781e728
Vulpelo <damian.praski@gmail.com>
2023-04-05 16:45:11 +0200

    modified plugin's page, added checkbox for new function

--- src/popup.js
+++ src/popup.js
@@ -13,13 +13,21 @@ window.onload = function() {
         hideYTShortsTabInput.addEventListener("input", function(e) {
             chrome.storage.local.set({hideYTShortsTab: e.target.checked});
         })
+
+        let rearrangeVideosAfterHidingAShortInput = document.getElementById("rearrangeVideosAfterHidingAShortInput");
+        rearrangeVideosAfterHidingAShortInput.checked = value.rearrangeVideosAfterHidingAShort;
+        rearrangeVideosAfterHidingAShortInput.addEventListener("input", function(e) {
+            chrome.storage.local.set({rearrangeVideosAfterHidingAShort: e.target.checked});
+        })
     });
 
     // Set language
     document.getElementById("hide_videos_text").textContent=chrome.i18n.getMessage("cfg_hide_videos");
     document.getElementById("hide_tab_text").textContent=chrome.i18n.getMessage("cfg_hide_tab");
     document.getElementById("settingsText").textContent=chrome.i18n.getMessage("setting_text");
-
+    document.getElementById("fill_gaps_text").textContent=chrome.i18n.getMessage("cfg_fill_gaps");
+    document.getElementById("fill_gaps_tooltip_text").textContent=chrome.i18n.getMessage("cfg_fill_gaps_tooltip");
+    
     // version
     let manifestData = chrome.runtime.getManifest();
     document.getElementById("ext-version").textContent = "v" + manifestData.version;
