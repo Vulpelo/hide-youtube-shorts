1359226e3f9dca669943bbdb396eec9fb32bbb58
Vulpelo <damian.praski@gmail.com>
2023-05-14 12:03:03 +0200

    FIxed accesing class variable

--- src/HidingShortsWithContainer.js
+++ src/HidingShortsWithContainer.js
@@ -16,7 +16,7 @@ class HidingShortsWithContainer {
     return true;
   }
 
-  findParentWithShelfTag(element) { 
+  findParentWithShelfTag(element) {
     while (element.tagName.toLowerCase() != this.shelfWithVideosTag && element.parentElement != null) {
       element = element.parentElement;
     }
@@ -28,7 +28,7 @@ class HidingShortsWithContainer {
     elements.forEach(element => {
       // hide whole shelf if just contains "ytd-reel-item-renderer" tag. For now seems to be only used for yt-shorts videos
       // and hide any video container that contains a ref link to shorts
-      if (element.innerHTML.search("href=\"/shorts/") == -1) return; 
+      if (element.innerHTML.search("href=\"/shorts/") == -1) return;
       element.setAttribute("hidden", true);
 
       // check if all children are hidden, if yes then find parent with tag "ytd-shelf-renderer" and hide that too
@@ -60,8 +60,8 @@ class HidingShortsWithContainer {
 
 class OperationsAfterHidingElement {
   rearrangeVideosAfterHidingAShort = false;
+  // for elements with ytd-rich-item-renderer
   RICH_GRID_ROW = "ytd-rich-grid-row";
-  RICH_ITEM_RENDERER = "ytd-rich-item-renderer";
 
   countVisibleElementsInRow(row) {
     let visibleCount = 0;
@@ -86,7 +86,7 @@ class OperationsAfterHidingElement {
       for (let i = startIndex; i < richGridRows.length - 1; i++) {
         // assuming next row always has child and is visible.
         const nextRitchRowDiv = richGridRows[i + 1].querySelector("div");
-        if (nextRitchRowDiv.childElementCount <= 0) 
+        if (nextRitchRowDiv.childElementCount <= 0)
           break;
         richGridRows[i].querySelector("div").appendChild(nextRitchRowDiv.childNodes[0]);
       }
@@ -95,8 +95,9 @@ class OperationsAfterHidingElement {
   /*!rearranging video elements in richGridRows */
 
   doOperations(element) {
-    if (rearrangeVideosAfterHidingAShort && element.parentElement.parentElement.tagName.toLowerCase().match(RICH_GRID_ROW)) {
-      if (element.hasAttribute("items-per-row")) {
+    if (this.rearrangeVideosAfterHidingAShort) {
+      if (element.parentElement.parentElement.tagName.toLowerCase().match(RICH_GRID_ROW) &&
+        element.hasAttribute("items-per-row")) {
         rearrangeVideosInRichGridRows(element.parentElement, element.getAttribute("items-per-row"));
       }
     }
