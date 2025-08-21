334c4c5e0601cc9472024c4b516fa6065d0c482c
Vulpelo <damian.praski@gmail.com>
2025-08-21 18:56:36 +0200

    #57 - updated queryselector for getting video's length; including videos from channel's search to also allow them to be hidden using "hide videos shorter than" option; remove container if all inner videos are hidden; formatted files;

--- src/HidingShortsMore.js
+++ src/HidingShortsMore.js
@@ -1,104 +1,111 @@
 
-// hide video renderer and its parent shelf renderer if it only contains 1 video renderer
-class HidingShortsWithContainer {
-  elementTagName = "ytd-video-renderer";
-  shelfWithVideosTag = "ytd-shelf-renderer";
-
-  constructor(querySelectorVideo, shelfWithVideosTag) {
-    this.elementTagName = querySelectorVideo;
-    this.shelfWithVideosTag = shelfWithVideosTag;
-  }
-
-  allChildrenHidden(element) {
-    for (var i = 0; i < element.childElementCount; i++) {
-      if (!element.children[i].hasAttribute("hidden")) return false;
-    }
-    return true;
-  }
-
-  findParentWithShelfTag(element) {
-    while (element.tagName.toLowerCase() != this.shelfWithVideosTag && element.parentElement != null) {
-      element = element.parentElement;
-    }
-    return element.tagName.toLowerCase() == this.shelfWithVideosTag ? element : null;
-  }
-
-  hideShort(element) {
-    // hide whole shelf if just contains "ytd-reel-item-renderer" tag. For now seems to be only used for yt-shorts videos
-    // and hide any video container that contains a ref link to shorts
-    if (element.querySelector('[href^="/shorts/"]') == null) return;
-    element.setAttribute("hidden", true);
-
-    // check if all children are hidden, if yes then find parent with tag "ytd-shelf-renderer" and hide that too
-    if (this.allChildrenHidden(element.parentElement)) {
-      const parent = this.findParentWithShelfTag(element.parentElement);
-      if (parent == null) return;
-      parent.setAttribute("hidden", true);
-    }
-  }
-
-  showShort(element) {
-    // hide whole shelf if just contains "ytd-reel-item-renderer" tag. For now seems to be only used for yt-shorts videos
-    // and hide any video container that contains a ref link to shorts
-    if (element.querySelector('[href^="/shorts/"]') != null) {
-      if (element.hasAttribute("hidden")) {
-        element.removeAttribute("hidden");
-
-        const parent = this.findParentWithShelfTag(element.parentElement);
-        if (parent == null || !parent.hasAttribute("hidden")) return;
-        parent.removeAttribute("hidden");
-      }
-    }
-  }
+function _allChildrenHidden(element) {
+	for (var i = 0; i < element.childElementCount; i++) {
+		if (!element.children[i].hasAttribute("hidden")) return false;
+	}
+	return true;
 }
 
-class OperationsAfterHidingElement {
-  // for elements with ytd-rich-item-renderer
-  RICH_GRID_ROW = "ytd-rich-grid-row";
+function _findParentWithElementTag(element, container) {
+	while (element.tagName.toLowerCase() != container && element.parentElement != null) {
+		element = element.parentElement;
+	}
+	return element.tagName.toLowerCase() == container ? element : null;
+}
 
-  countVisibleElementsInRow(row) {
-    let visibleCount = 0;
-    for (const child of row.children) {
-      if (!child.hasAttribute("hidden")) {
-        visibleCount++;
-      }
-    }
-    return visibleCount;
-  }
+function hideContainerOfElement(container, element) {
+	if (_allChildrenHidden(element.parentElement)) {
+		const parent = _findParentWithElementTag(element.parentElement, container);
+		if (parent == null) return;
+		parent.setAttribute("hidden", true);
+	}
+}
 
-  rearrangeVideosInRichGridRows(startFromRowElement, elementsPerRow) {
-    // each rich_grid_row has a div element inside, and IT contains list of videos
-    const richGridRows = startFromRowElement.parentElement.parentElement.querySelectorAll(this.RICH_GRID_ROW);
-    const startIndex = Array.from(richGridRows).indexOf(startFromRowElement.parentElement);
+function showContainerOfElement(container, element) {
+	const parent = _findParentWithElementTag(element.parentElement, container);
+	if (parent == null || !parent.hasAttribute("hidden")) return;
+	parent.removeAttribute("hidden");
+}
 
-    const amountOfVisibleElements = this.countVisibleElementsInRow(startFromRowElement);
-    const elementsToMove = elementsPerRow - amountOfVisibleElements;
 
-    for (let j = 0; j < elementsToMove; j++) {
-      // for each next row, move one element to previous row
-      for (let i = startIndex; i < richGridRows.length - 1; i++) {
-        // assuming next row always has child and is visible.
-        const nextRitchRowDiv = richGridRows[i + 1].querySelector("div");
-        if (nextRitchRowDiv.childElementCount <= 0)
-          break;
-        richGridRows[i].querySelector("div").appendChild(nextRitchRowDiv.childNodes[0]);
-      }
-    }
-  }
+// hide video renderer and its parent shelf renderer if it only contains 1 video renderer
+class HidingShortsWithContainer {
+	container = "ytd-shelf-renderer";
+	elementTagName = "ytd-video-renderer";
 
-  doOperations(element) {
-    if (element.parentElement.parentElement.tagName.toLowerCase().match(this.RICH_GRID_ROW)
-        && element.hasAttribute("items-per-row")) {
-      const pElement = element.parentElement;
-      const itemsPerRow = element.getAttribute("items-per-row");
-      element.remove();
-      this.rearrangeVideosInRichGridRows(pElement, itemsPerRow);
-    }
-  }
+	constructor(querySelectorVideo, shelfWithVideosTag) {
+		this.container = shelfWithVideosTag
+		this.elementTagName = querySelectorVideo;
+	}
+
+	hideShort(element) {
+		// hide whole shelf if just contains "ytd-reel-item-renderer" tag. For now seems to be only used for yt-shorts videos
+		// and hide any video container that contains a ref link to shorts
+		if (element.querySelector('[href^="/shorts/"]') == null) return false;
+		element.setAttribute("hidden", true);
+
+		hideContainerOfElement(this.container, element);
+		return true;
+	}
+
+	showShort(element) {
+		// hide whole shelf if just contains "ytd-reel-item-renderer" tag. For now seems to be only used for yt-shorts videos
+		// and hide any video container that contains a ref link to shorts
+		if (element.querySelector('[href^="/shorts/"]') == null) return;
+		if (element.hasAttribute("hidden")) {
+			element.removeAttribute("hidden");
+			showContainerOfElement(this.container, element);
+		}
+	}
+}
+
+class RearrangeVideosInGrid {
+	// for elements with ytd-rich-item-renderer
+	RICH_GRID_ROW = "ytd-rich-grid-row";
+
+	_countVisibleElementsInRow(row) {
+		let visibleCount = 0;
+		for (const child of row.children) {
+			if (!child.hasAttribute("hidden")) {
+				visibleCount++;
+			}
+		}
+		return visibleCount;
+	}
+
+	_rearrangeVideosInRichGridRows(startFromRowElement, elementsPerRow) {
+		// each rich_grid_row has a div element inside, and IT contains list of videos
+		const richGridRows = startFromRowElement.parentElement.parentElement.querySelectorAll(this.RICH_GRID_ROW);
+		const startIndex = Array.from(richGridRows).indexOf(startFromRowElement.parentElement);
+
+		const amountOfVisibleElements = this._countVisibleElementsInRow(startFromRowElement);
+		const elementsToMove = elementsPerRow - amountOfVisibleElements;
+
+		for (let j = 0; j < elementsToMove; j++) {
+			// for each next row, move one element to previous row
+			for (let i = startIndex; i < richGridRows.length - 1; i++) {
+				// assuming next row always has child and is visible.
+				const nextRitchRowDiv = richGridRows[i + 1].querySelector("div");
+				if (nextRitchRowDiv.childElementCount <= 0)
+					break;
+				richGridRows[i].querySelector("div").appendChild(nextRitchRowDiv.childNodes[0]);
+			}
+		}
+	}
+
+	execute(element) {
+		if (element.parentElement.parentElement.tagName.toLowerCase().match(this.RICH_GRID_ROW)
+			&& element.hasAttribute("items-per-row")) {
+			const pElement = element.parentElement;
+			const itemsPerRow = element.getAttribute("items-per-row");
+			element.remove();
+			this._rearrangeVideosInRichGridRows(pElement, itemsPerRow);
+		}
+	}
 }
 
 function insertCloseShelfButton(element) {
-  element.insertAdjacentHTML("afterbegin", `
+	element.insertAdjacentHTML("afterbegin", `
     <div id="shelfCloseButton" class="style-scope ytd-menu-renderer">
       <a class="yt-spec-button-shape-next yt-spec-button-shape-next--text yt-spec-button-shape-next--call-to-action yt-spec-button-shape-next--size-m "
         aria-label="Close" rel="nofollow" force-new-state="true"
