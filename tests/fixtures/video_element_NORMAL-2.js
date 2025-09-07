b7cf6605b1c1f24e171fe183a5427177aeb54d57
Vulpelo <damian.praski@gmail.com>
2025-09-07 14:34:55 +0200

    [bugfix] 59 - hiding new node elements of videos. Fixing hding LIVE and UPCOMING videos

---
+++ tests/fixtures/video_element_NORMAL-2.js
@@ -0,0 +1,130 @@
+const element = `
+<ytd-rich-item-renderer class="style-scope ytd-rich-grid-renderer" items-per-row="3" lockup="true"
+    rendered-from-rich-grid=""
+    is-in-first-column=""><!--css-build:shady--><!--css_build_scope:ytd-rich-item-renderer--><!--css_build_styles:video.youtube.src.web.polymer.shared.ui.styles.yt_base_styles.yt.base.styles.css.js-->
+    <div id="content" class="style-scope ytd-rich-item-renderer"><yt-lockup-view-model
+            class="ytd-rich-item-renderer lockup">
+            <div
+                class="yt-lockup-view-model yt-lockup-view-model--vertical content-id-PlWHeR6k_VY yt-lockup-view-model--rich-grid-legacy-margin">
+                <a href="/watch?v=PlWHeR6k_VY" class="yt-lockup-view-model__content-image" style=""
+                    aria-haspopup="false" tabindex="-1" aria-hidden="true"><yt-thumbnail-view-model
+                        class="ytThumbnailViewModelHost ytThumbnailViewModelAspectRatio16By9 ytThumbnailViewModelLarge">
+                        <div class="ytThumbnailViewModelImage">
+                            <img alt="" class="ytCoreImageHost ytCoreImageFillParentHeight ytCoreImageFillParentWidth ytCoreImageContentModeScaleAspectFill ytCoreImageLoaded"
+                                src="https://i.ytimg.com/vi/PlWHeR6k_VY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&amp;rs=AOn4CLCT-coXXldpK5F7V3hqIgeiF6R63A">
+                        </div>
+                        <yt-thumbnail-overlay-badge-view-model class="yt-thumbnail-overlay-badge-view-model yt-thumbnail-overlay-badge-view-model--bottom-end yt-thumbnail-overlay-badge-view-model--large">
+                        <yt-thumbnail-badge-view-model class="ytThumbnailBadgeViewModelHost yt-thumbnail-overlay-badge-view-model__badge">
+                        <badge-shape class="yt-badge-shape yt-badge-shape--thumbnail-default yt-badge-shape--thumbnail-badge">
+                                    <div class="yt-badge-shape__text">1:47</div>
+                                </badge-shape></yt-thumbnail-badge-view-model></yt-thumbnail-overlay-badge-view-model><!---->
+                    </yt-thumbnail-view-model></a>
+                <div class="yt-lockup-view-model__metadata"><yt-lockup-metadata-view-model
+                        class="yt-lockup-metadata-view-model yt-lockup-metadata-view-model--vertical yt-lockup-metadata-view-model--standard yt-lockup-metadata-view-model--typography-bump yt-lockup-metadata-view-model--rich-grid-legacy-typography">
+                        <div class="yt-lockup-metadata-view-model__avatar"><yt-decorated-avatar-view-model
+                                class="ytDecoratedAvatarViewModelHost"><yt-avatar-shape>
+                                    <div class="yt-spec-avatar-shape yt-spec-avatar-shape__button yt-spec-avatar-shape__button--button-medium yt-spec-avatar-shape__button--tappable"
+                                        aria-label="Przejdź na kanał" role="button" tabindex="0">
+                                        <div class="">
+                                            <div class="yt-spec-avatar-shape--avatar-size-medium"><img alt=""
+                                                    class="ytCoreImageHost yt-spec-avatar-shape__image ytCoreImageFillParentHeight ytCoreImageFillParentWidth ytCoreImageContentModeScaleToFill ytCoreImageLoaded"
+                                                    src="https://yt3.ggpht.com/_7NuLsyaCWL63spCS74vy7RGd8s5cwx0FATJTvEeew3OH5d7TuD9hGcxbrhoYcMFT4mBxaJm=s68-c-k-c0x00ffffff-no-rj">
+                                                <div
+                                                    class="yt-spec-avatar-shape__image-overlays yt-spec-avatar-shape__image">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </yt-avatar-shape></yt-decorated-avatar-view-model></div>
+                        <div class="yt-lockup-metadata-view-model__text-container">
+                            <h3 class="yt-lockup-metadata-view-model__heading-reset" title="So It Begins... ">
+                                <a href="/watch?v=PlWHeR6k_VY" class="yt-lockup-metadata-view-model__title" style=""
+                                    aria-haspopup="false" aria-label="So It Begins...  8 minut i 47 sekund"><span
+                                        class="yt-core-attributed-string yt-core-attributed-string--white-space-pre-wrap"
+                                        dir="auto" role="text">So It Begins... </span></a></h3>
+                            <div class="yt-lockup-metadata-view-model__metadata"><yt-content-metadata-view-model
+                                    class="yt-content-metadata-view-model yt-content-metadata-view-model--medium-text">
+                                    <div class="yt-content-metadata-view-model__metadata-row"><span
+                                            class="yt-core-attributed-string yt-content-metadata-view-model__metadata-text yt-core-attributed-string--white-space-pre-wrap yt-core-attributed-string--link-inherit-color"
+                                            dir="auto"><span class="" style="font-weight: 400;" dir="auto"><a
+                                                    class="yt-core-attributed-string__link yt-core-attributed-string__link--call-to-action-color yt-core-attributed-string--link-inherit-color"
+                                                    tabindex="0" href="/@techlinked" target=""
+                                                    force-new-state="true">TechLinked
+                                                    <span
+                                                        class="yt-core-attributed-string--link-inherit-color"
+                                                        style="color: rgb(170, 170, 170);" dir="auto"><span
+                                                            class="yt-core-attributed-string--inline-block-mod"
+                                                            style="margin-left: 4px;"><span
+                                                                class="ytIconWrapperHost yt-core-attributed-string__image-element yt-core-attributed-string__image-element--image-alignment-vertical-center"
+                                                                role="img" aria-label="" aria-hidden="true"
+                                                                style="height: 14px; width: 14px;"><span
+                                                                    class="yt-icon-shape ytSpecIconShapeHost">
+                                                                    <div
+                                                                        style="width: 100%; height: 100%; display: block; fill: currentcolor;">
+                                                                        <svg xmlns="http://www.w3.org/2000/svg"
+                                                                            height="24" viewBox="0 0 24 24" width="24"
+                                                                            focusable="false" aria-hidden="true"
+                                                                            style="pointer-events: none; display: inherit; width: 100%; height: 100%;">
+                                                                            <path
+                                                                                d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10 10-4.5 10-10S17.5 2 12 2zM9.8 17.3l-4.2-4.1L7 11.8l2.8 2.7L17 7.4l1.4 1.4-8.6 8.5z">
+                                                                            </path>
+                                                                        </svg></div>
+                                                                </span></span></span></span>
+                                                            </a>
+                                                            </span></span></div>
+                                    <div class="yt-content-metadata-view-model__metadata-row"><span
+                                            class="yt-core-attributed-string yt-content-metadata-view-model__metadata-text yt-core-attributed-string--white-space-pre-wrap yt-core-attributed-string--link-inherit-color"
+                                            dir="auto" role="text">477&nbsp;tys. wyświetleń</span><span
+                                            aria-hidden="true" class="yt-content-metadata-view-model__delimiter"> •
+                                        </span><span
+                                            class="yt-core-attributed-string yt-content-metadata-view-model__metadata-text yt-core-attributed-string--white-space-pre-wrap yt-core-attributed-string--link-inherit-color"
+                                            dir="auto" role="text">2 tygodnie temu</span></div>
+                                </yt-content-metadata-view-model></div>
+                        </div>
+                        <div class="yt-lockup-metadata-view-model__menu-button"><button-view-model
+                                class="ytSpecButtonViewModelHost"><button
+                                    class="yt-spec-button-shape-next yt-spec-button-shape-next--text yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m yt-spec-button-shape-next--icon-button yt-spec-button-shape-next--enable-backdrop-filter-experiment"
+                                    title="" style="" aria-label="Więcej działań" aria-disabled="false">
+                                    <div aria-hidden="true" class="yt-spec-button-shape-next__icon"><span
+                                            class="ytIconWrapperHost" style="width: 24px; height: 24px;"><span
+                                                class="yt-icon-shape ytSpecIconShapeHost">
+                                                <div
+                                                    style="width: 100%; height: 100%; display: block; fill: currentcolor;">
+                                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                                        enable-background="new 0 0 24 24" height="24"
+                                                        viewBox="0 0 24 24" width="24" focusable="false"
+                                                        aria-hidden="true"
+                                                        style="pointer-events: none; display: inherit; width: 100%; height: 100%;">
+                                                        <path
+                                                            d="M12 16.5c.83 0 1.5.67 1.5 1.5s-.67 1.5-1.5 1.5-1.5-.67-1.5-1.5.67-1.5 1.5-1.5zM10.5 12c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5-.67-1.5-1.5-1.5-1.5.67-1.5 1.5zm0-6c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5-.67-1.5-1.5-1.5-1.5.67-1.5 1.5z">
+                                                        </path>
+                                                    </svg></div>
+                                            </span></span></div><yt-touch-feedback-shape
+                                        style="border-radius: inherit;">
+                                        <div aria-hidden="true"
+                                            class="yt-spec-touch-feedback-shape yt-spec-touch-feedback-shape--touch-response">
+                                            <div class="yt-spec-touch-feedback-shape__stroke"></div>
+                                            <div class="yt-spec-touch-feedback-shape__fill"></div>
+                                        </div>
+                                    </yt-touch-feedback-shape>
+                                </button></button-view-model></div>
+                    </yt-lockup-metadata-view-model>
+                </div>
+                <yt-touch-feedback-shape style="border-radius: inherit;">
+                    <div aria-hidden="true"
+                        class="yt-spec-touch-feedback-shape yt-spec-touch-feedback-shape--touch-response yt-spec-touch-feedback-shape--thumbnail-size-small yt-spec-touch-feedback-shape--trigger-events">
+                        <div class="yt-spec-touch-feedback-shape__stroke"></div>
+                        <div class="yt-spec-touch-feedback-shape__fill"></div>
+                    </div>
+                </yt-touch-feedback-shape>
+            </div>
+        </yt-lockup-view-model></div>
+    <yt-interaction id="interaction" class="extended rounded-large style-scope ytd-rich-item-renderer"
+        hidden=""><!--css-build:shady--><!--css_build_scope:yt-interaction--><!--css_build_styles:video.youtube.src.web.polymer.shared.ui.styles.yt_base_styles.yt.base.styles.css.js,video.youtube.src.web.polymer.shared.ui.yt_interaction.yt.interaction.css.js-->
+        <div class="stroke style-scope yt-interaction"></div>
+        <div class="fill style-scope yt-interaction"></div>
+    </yt-interaction>
+</ytd-rich-item-renderer>
+`
+
+module.exports = element
